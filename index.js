29ab47526bc71cf655ec61f525dba0511fb3cac3
Maaruf Dauda <mkdauda.work@gmail.com>
2019-12-13 12:53:26 +0100

    :art: Create config folder
    export environment variables from index.js in config folder

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
 
+const { port } = require('./config/index');
+
 const app = require('./api/server')
 
-const port = process.env.PORT
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
