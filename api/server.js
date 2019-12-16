5dbcc5706b62c7c2ed7ceed7035f59638f8426ba
Maaruf Dauda <mkdauda.work@gmail.com>
2019-12-16 22:21:10 +0100

    :sparkles: Implement authRouter in server.js

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const authRouter = require('../resources/auth/router');
 
 const server = express();
 
@@ -8,6 +9,7 @@ server.use(express.json());
 server.use(cors());
 server.use(helmet());
 
+server.use('/api/auth', authRouter);
 server.get('/', (req, res) => {
   res.status(200).json({ message: `Welcome to the QuickDecks API` });
 });
