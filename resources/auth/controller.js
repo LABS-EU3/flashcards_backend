63fb611f32bad8686ad2d3ab3effbe7426b90b64
Maaruf Dauda <mkdauda.work@gmail.com>
2019-12-16 22:02:20 +0100

    :sparkles: Implement db helper functions to create new user
    Check user does not already exist and create new user in database if so. Also add new rule to eslint to allow snake_case destructuring

--- resources/auth/controller.js
+++ resources/auth/controller.js
@@ -1,4 +1,7 @@
 const joi = require('@hapi/joi');
+const bcrypt = require('bcrypt');
+
+const model = require('./model');
 
 const schema = joi.object({
   email: joi
@@ -21,6 +24,30 @@ const schema = joi.object({
 exports.signup = async (req, res) => {
   try {
     await schema.validateAsync(req.body);
+
+    const { email, full_name, image_url, password } = req.body;
+
+    const userExists = await model.filter({ email });
+    if (userExists) {
+      res
+        .status(400)
+        .json({ error: true, message: `User with this email already exists` });
+    }
+    const hashedPassword = bcrypt.hashSync(password, 10);
+
+    const userCreated = model.createUser({
+      email,
+      password: hashedPassword,
+      full_name,
+      image_url,
+      isConfirmed: false,
+    });
+
+    res.status(201).json({
+      error: false,
+      message: `User created successfully`,
+      data: userCreated,
+    });
   } catch (error) {
     res.status(400).json({ error: true, message: error.message, data: error });
   }
