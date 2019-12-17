e5f7b926ca9c7eb55b959a752ae11e0e8e4b6516
Anna Winther <anski.anna@gmail.com>
2019-12-17 16:33:09 +0100

    Add validation for email on login. Check if email does exist

--- resources/auth/middlewares.js
+++ resources/auth/middlewares.js
@@ -4,7 +4,18 @@ exports.checkUserExists = async (req, res, next) => {
   const { email } = req.body;
   const userExists = await model.filter({ email });
   if (userExists) {
-    res.status(403).json({ message: `User with this email already exists` });
+    res.status(400).json({ message: `User with this email already exists` });
+  } else {
+    next();
+  }
+};
+
+exports.checkEmailExists = async (req, res, next) => {
+  const { email } = req.body;
+  const emailExists = await model.filter({ email });
+  /* check email exists -> next, */
+  if (!emailExists) {
+    res.status(404).json({ message: 'User with this email does not exists' });
   } else {
     next();
   }
