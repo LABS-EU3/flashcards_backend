f74fb7f0b3efae2281e8b3b77863784e70a33c80
Maaruf Dauda <mkdauda.work@gmail.com>
2019-12-17 13:03:49 +0100

    :sparkles: Use middleware to ensure email uniqueness

--- resources/auth/router.js
+++ resources/auth/router.js
@@ -3,9 +3,10 @@ const { signup } = require('./controller');
 
 const validate = require('../../utils/validate');
 const authSchema = require('./authSchema');
+const { checkUserExists } = require('./middlewares');
 
 const authRouter = express.Router();
 
-authRouter.post('/register', validate(authSchema), signup);
+authRouter.post('/register', validate(authSchema), checkUserExists, signup);
 
 module.exports = authRouter;
