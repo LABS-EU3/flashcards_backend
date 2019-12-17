4790f1575e105659f368f1fef154ffd83b3961b5
Maaruf Dauda <mkdauda.work@gmail.com>
2019-12-17 13:04:19 +0100

    :bug: Correctly export check email middleware

--- resources/auth/middlewares.js
+++ resources/auth/middlewares.js
@@ -1,10 +1,10 @@
 const model = require('./model');
 
-module.checkUserExists = async (req, res, next) => {
+exports.checkUserExists = async (req, res, next) => {
   const { email } = req.body;
   const userExists = await model.filter({ email });
   if (userExists) {
-    res.status(400).json({ message: `User with this email already exists` });
+    res.status(403).json({ message: `User with this email already exists` });
   } else {
     next();
   }
