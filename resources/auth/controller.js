2a2070f49b70c5969b5db581e357ce6f4754a60d
Anna Winther <anski.anna@gmail.com>
2019-12-17 15:42:08 +0100

    Fixed typo

--- resources/auth/controller.js
+++ resources/auth/controller.js
@@ -36,11 +36,11 @@ exports.login = async (req, res) => {
     if (user && bcrypt.compareSync(password, user.password)) {
       /* 1st is user submitted password. 2nd is hashed stored password */
 
-      const tokenD = generateToken(user);
+      const token = generateToken(user);
 
       res.status(200).json({
         message: `Welcome. You're logged in!`,
-        data: { tokenD, user },
+        data: { token, user },
       });
     } else {
       res.status(401).json({ message: 'Invalid credentials' });
