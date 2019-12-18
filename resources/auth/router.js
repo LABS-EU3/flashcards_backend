fbf01f28056ab53a4066ea050846f9798d4742d5
Anna Winther <anski.anna@gmail.com>
2019-12-18 17:42:07 +0100

    Add route for forgot_password and check_valid_token

--- resources/auth/router.js
+++ resources/auth/router.js
@@ -1,13 +1,28 @@
 const express = require('express');
-const { signup, login } = require('./controller');
+const {
+  signup,
+  login,
+  requestResetToken,
+  checkResetToken,
+} = require('./controller');
 
 const validate = require('../../utils/validate');
-const { signUpSchema, loginSchema } = require('./authSchema');
+const {
+  signUpSchema,
+  loginSchema,
+  forgotPasswordSchema,
+} = require('./authSchema');
 const { checkUserExists, checkEmailExists } = require('./middlewares');
 
 const authRouter = express.Router();
 
 authRouter.post('/register', validate(signUpSchema), checkUserExists, signup);
 authRouter.post('/login', validate(loginSchema), checkEmailExists, login);
+authRouter.post(
+  '/forgot_password',
+  validate(forgotPasswordSchema),
+  requestResetToken
+);
+authRouter.post('/check_valid_token', checkResetToken);
 
 module.exports = authRouter;
