450f6c70b5656e0f80ba3f0c1c4f1eec57526e1f
Maaruf Dauda <mkdauda.work@gmail.com>
2019-12-18 12:26:50 +0100

    :bento: Add emailAddres and password to config.index.js

--- config/index.js
+++ config/index.js
@@ -1,6 +1,10 @@
 const port = process.env.PORT || 4003;
 const SECRET = process.env.SECRET || 'A very secure secret';
+const emailAddress = process.env.NODEMAILER_EMAIL_ADDRESS;
+const password = process.env.NODEMAILER_EMAIL_PASSWORD;
 module.exports = {
   port,
   SECRET,
+  emailAddress,
+  password,
 };
