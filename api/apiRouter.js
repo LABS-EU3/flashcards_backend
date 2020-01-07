655514e717b8f17bf04e038745fd7f32af8756c1
richanynguon <alina.nguon@gmail.com>
2020-01-07 10:56:39 -0500

    connected flashcards router to server

--- api/apiRouter.js
+++ api/apiRouter.js
@@ -1,7 +1,9 @@
 const appRouter = require('express').Router();
 
 const authRouter = require('../resources/auth/router');
+const flashcardsRouter = require('../resources/flashcards/flashcardsRouter');
 
 appRouter.use('/auth', authRouter);
+appRouter.use('/card', flashcardsRouter);
 
 module.exports = appRouter;
