feaadc3738c6942a4e439e02e5645fb0950c1241
richanynguon <alina.nguon@gmail.com>
2020-01-09 16:02:23 -0500

    create tags table and connector table with seed data

---
+++ data/migrations/20200109154014_build-deck-tag-table.js
@@ -0,0 +1,26 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('deck_tags', table => {
+    table.increments();
+    table
+      .integer('deck_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('decks')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+    table
+      .integer('tag_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('tags')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+    table.string('name').notNullable();
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('deck_tags');
+};
