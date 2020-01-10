7c7f302713890f26d68bc087ae9d4bfb7a7ee7d0
Omulosi <mulongojohnpaul@gmail.com>
2020-01-10 14:46:52 +0300

    Update deckExists middleware
    This removes the  'Cannot set headers after they are sent to the client'
    error.

--- resources/decks/middlewares.js
+++ resources/decks/middlewares.js
@@ -4,12 +4,12 @@ exports.deckExists = async (req, res, next) => {
   const { id } = req.params;
   const deck = await findById(id);
 
-  if (!deck) {
+  if (deck) {
+    next();
+  } else {
     res.status(404).json({
       status: 404,
       message: 'Deck ID does not exist',
     });
   }
-
-  next();
 };
