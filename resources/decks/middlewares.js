5c4eef18f9684590f2dd5f6ad788776ab490a18f
Omulosi <mulongojohnpaul@gmail.com>
2020-01-10 14:10:08 +0300

    Update deck exists middleware
    Ensure the middlewre checks for deck existence and returns
    a Not Found response if deck does not exist

--- resources/decks/middlewares.js
+++ resources/decks/middlewares.js
@@ -2,12 +2,14 @@ const { findById } = require('../decks/model');
 
 exports.deckExists = async (req, res, next) => {
   const { id } = req.params;
-  try {
-    await findById(id);
-    next();
-  } catch (error) {
-    res.status(500).json({
-      message: `Unable to process id`,
+  const deck = await findById(id);
+
+  if (!deck) {
+    res.status(404).json({
+      status: 404,
+      message: 'Deck ID does not exist',
     });
   }
+
+  next();
 };
