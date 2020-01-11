9f24c7fbfc983f043d3ebd79acd9a1591b7b6dff
Noble Obioma <nobioma1@gmail.com>
2020-01-11 20:42:16 +0100

    Create seeds for users, decks and cards

--- data/seeds/02-flashcards-data.js
+++ data/seeds/02-flashcards-data.js
@@ -4,7 +4,6 @@ exports.seed = function(knex) {
     .then(function() {
       return knex('flashcards').insert([
         {
-          id: 2,
           deck_id: 1,
           user_id: 1,
           question: 'What is data mining?',
@@ -17,7 +16,6 @@ exports.seed = function(knex) {
           updated_at: '2020-01-08T10:44:38.761Z',
         },
         {
-          id: 3,
           deck_id: 1,
           user_id: 1,
           question: 'Hey Anna hehe sup',
@@ -27,34 +25,89 @@ exports.seed = function(knex) {
           updated_at: '2020-01-08T10:45:05.269Z',
         },
         {
-          id: 5,
           deck_id: 2,
-          user_id: 1,
-          question: 'here is my question answer me',
-          answer: 'here is my answer question me',
+          user_id: 2,
+          question: 'Which of the following is used in pencils?',
+          answer: 'Graphite',
           image_url: null,
-          created_at: '2020-01-08T11:34:52.174Z',
-          updated_at: '2020-01-08T11:34:52.174Z',
         },
         {
-          id: 6,
           deck_id: 2,
-          user_id: 1,
-          question: 'here is my question answer me',
-          answer: 'here is my answer question me',
+          user_id: 2,
+          question:
+            'Brass gets discoloured in air because of the presence of which of the following gases in air?',
+          answer: 'Hydrogen sulphide',
           image_url: null,
-          created_at: '2020-01-08T11:34:52.174Z',
-          updated_at: '2020-01-08T11:34:52.174Z',
         },
         {
-          id: 8,
           deck_id: 3,
-          user_id: 1,
+          user_id: 3,
+          question:
+            'In which decade was the American Institute of Electrical Engineers (AIEE) founded?',
+          answer: '1880s',
+          image_url: null,
+        },
+        {
+          deck_id: 3,
+          user_id: 3,
+          question:
+            'What is part of a database that holds only one type of information?',
+          answer: 'Field',
+          image_url: null,
+        },
+        {
+          deck_id: 4,
+          user_id: 4,
+          question:
+            'Ordinary table salt is sodium chloride. What is baking soda?',
+          answer: 'Sodium bicarbonate',
+          image_url: null,
+        },
+        {
+          deck_id: 4,
+          user_id: 4,
+          question: 'Plants receive their nutrients from the?',
+          answer: 'Sun',
+          image_url: null,
+        },
+        {
+          deck_id: 5,
+          user_id: 5,
+          question:
+            'Grand Central Terminal, Park Avenue, New York is the world...?',
+          answer: 'largest railway station',
+          image_url: null,
+        },
+        {
+          deck_id: 5,
+          user_id: 5,
+          question:
+            'For which of the following disciplines is Nobel Prize awarded?',
+          answer:
+            'Physics and Chemistry, Physiology or Medicine, Literature, Peace and Economics',
+          image_url: null,
+        },
+        {
+          deck_id: 6,
+          user_id: 6,
+          question: 'Who is the father of Geometry?',
+          answer: 'Euclid',
+          image_url: null,
+        },
+        {
+          deck_id: 6,
+          user_id: 6,
+          question:
+            'The Indian to beat the computers in mathematical wizardry is',
+          answer: 'Shakunthala Devi',
+          image_url: null,
+        },
+        {
+          deck_id: 7,
+          user_id: 7,
           question: 'here is my question answer me',
           answer: 'here is my answer question me',
           image_url: null,
-          created_at: '2020-01-08T11:34:52.174Z',
-          updated_at: '2020-01-08T11:34:52.174Z',
         },
       ]);
     });
