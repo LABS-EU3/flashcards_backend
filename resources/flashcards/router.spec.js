f60677f34b34aea231384b011c561eb4dd387b7b
Noble Obioma <nobioma1@gmail.com>
2020-01-11 20:48:47 +0100

    Fix Tests

--- resources/flashcards/router.spec.js
+++ resources/flashcards/router.spec.js
@@ -1,106 +1,176 @@
 const request = require('supertest');
 const server = require('../../api/server');
-const deckModel = require('../decks/model');
-const userModel = require('../auth/model');
 const db = require('../../data/dbConfig');
 
 const userObject = {
-  email: 'h.kakashi@gmail.com',
-  password: 'aVeryLongPassword',
-  full_name: 'Hatake Kakashi',
-  image_url: 'google.com',
-  isConfirmed: false,
+  fullName: 'Test User',
+  email: 'testuser@xyz.com',
+  password: 'ALongSecurePassword',
+  imageUrl: 'google.com',
 };
 
-let user = {};
-let deck = {};
-
-beforeEach(async () => {
-  await db.raw('TRUNCATE TABLE users, decks CASCADE');
-  await userModel.createUser(userObject).then(async () => {
-    user = await userModel.filter({ email: userObject.email });
-    deckModel.add({
-      name: 'TestDeck',
-      user_id: user.id,
-    });
-    deck = await deckModel.findBy({ name: 'TestDeck' });
-  });
-});
-
-afterAll(async () => {
-  await db.destroy();
-});
-
-const flashcardObject = {
-  deckId: deck.id,
-  userId: user.id,
+let authToken;
+let user;
+const flashcard = {
+  deckId: null,
+  userId: null,
   questionText: 'When will AI takeover',
   answerText: 'What idk',
   imageUrl: 'https://robots.ieee.org/robots/cb2/Photos/SD/cb2-photo1-full.jpg',
 };
 
-// const editFlashcardObject = {
-//   questionText: 'When will AI takeover',
-//   answerText: 'It already has',
-//   imageUrl:
-// 'https://robots.ieee.org/robots/cb2/Photos/SD/cb2-photo1-full.jpg',
-// };
+beforeEach(async done => {
+  await db.raw('TRUNCATE TABLE users, decks, flashcards CASCADE');
+  const userRes = await request(server)
+    .post('/api/auth/register')
+    .send(userObject);
+
+  const deckRes = await request(server)
+    .post('/api/decks')
+    .send({ name: 'Example Deck' })
+    .set('Authorization', userRes.body.data.token);
+
+  authToken = userRes.body.data.token;
+  flashcard.deckId = deckRes.body.deck.id;
+  flashcard.userId = userRes.body.data.user.id;
+
+  user = userRes.body.data.user;
+  done();
+});
+
+afterAll(async done => {
+  await db.destroy();
+  done();
+});
 
 describe('Flashcards Router', () => {
   describe('[POST] /api/cards/', () => {
-    test('Returns 201 on success', async () => {
-      const res = await await request(server)
+    test('validation for card body', async done => {
+      const res = await request(server)
         .post('/api/cards')
-        .send(flashcardObject);
-      expect(res.status).toBe(201);
+        .send({})
+        .set('Authorization', authToken);
+      expect(res.status).toBe(400);
+      expect(res.body).toEqual({ error: '"Question Text" is required' });
+      done();
     });
 
-    test('Returns created user without their password', async () => {});
+    test('Create a new Flashcard', async done => {
+      const res = await request(server)
+        .post('/api/cards')
+        .send(flashcard)
+        .set('Authorization', authToken);
 
-    test('Token is returned on signup successful', async () => {});
-
-    test('Password is not stored in plain text', async () => {});
-
-    test('Email is required', async () => {});
-
-    test('Password is required', async () => {});
-
-    test('Full Name is required', async () => {});
-
-    test('imageUrl and isConfirmed are not required', async () => {});
-
-    test('Email cannot belong to multiple users', async () => {});
+      expect(res.status).toBe(201);
+      expect(res.body.card.question).toBe(flashcard.questionText);
+      expect(res.body.card.answer).toBe(flashcard.answerText);
+      expect(res.body.card.user_id).toBe(flashcard.userId);
+      done();
+    });
   });
 
   describe('[GET] /api/cards/users/:userId', () => {
-    test('Returns 200 OK on successful login', async () => {});
+    test('Should get all cards created by user', async done => {
+      await request(server)
+        .post('/api/cards')
+        .send(flashcard)
+        .set('Authorization', authToken);
+      await request(server)
+        .post('/api/cards')
+        .send({
+          ...flashcard,
+          questionText: 'Plants receive their nutrients from the?',
+          answerText: 'sun',
+        })
+        .set('Authorization', authToken);
 
-    test('Token is returned on successful login', async () => {});
+      const res = await request(server)
+        .get(`/api/cards/users/${user.id}`)
+        .set('Authorization', authToken);
 
-    test('Provided Email does not exist', async () => {});
-
-    test('Provided password is invalid', async () => {});
-
-    test('Email is required', async () => {});
-
-    test('Password is required', async () => {});
+      expect(res.status).toBe(200);
+      expect(res.body.cards).toHaveLength(2);
+      done();
+    });
   });
 
   describe('[GET] /api/cards/:id', () => {
-    test('Validation works', async () => {});
+    test('Get Card by ID', async done => {
+      const { body } = await request(server)
+        .post('/api/cards')
+        .send(flashcard)
+        .set('Authorization', authToken);
+
+      const res = await request(server)
+        .get(`/api/cards/${body.card.id}`)
+        .set('Authorization', authToken);
+
+      expect(res.status).toBe(200);
+      expect(res.body.card.question).toBe(flashcard.questionText);
+      done();
+    });
   });
 
   describe('[PUT] /api/cards/:id', () => {
-    test('Get email sent to reset password if forgotten', async () => {});
+    test('Update Card details', async done => {
+      const { body } = await request(server)
+        .post('/api/cards')
+        .send(flashcard)
+        .set('Authorization', authToken);
 
-    test('Email is sent', async () => {});
+      const res = await request(server)
+        .put(`/api/cards/${body.card.id}`)
+        .send({
+          ...flashcard,
+          questionText: 'Updated question text',
+          answerText: 'Updated answer text',
+        })
+        .set('Authorization', authToken);
 
-    test('Will not get an email sent if wrong email', async () => {});
+      expect(res.status).toBe(200);
+      expect(res.body.card.question).toBe('Updated question text');
+      expect(res.body.card.answer).toBe('Updated answer text');
+      done();
+    });
   });
 
   describe('[DELETE] /api/cards/:id', () => {
-    test('Cannot reset password if not valid token', async () => {});
+    test('Delete Card by Id', async done => {
+      await request(server)
+        .post('/api/cards')
+        .send({
+          ...flashcard,
+          questionText: 'Plants receive their nutrients from the?',
+          answerText: 'sun',
+        })
+        .set('Authorization', authToken);
 
-    test('User can reset password, testing if token is valid', async () => {});
+      await request(server)
+        .post('/api/cards')
+        .send({
+          ...flashcard,
+          questionText: 'Some question text',
+          answerText: 'Some answer text',
+        })
+        .set('Authorization', authToken);
+
+      const { body } = await request(server)
+        .post('/api/cards')
+        .send(flashcard)
+        .set('Authorization', authToken);
+
+      const res = await request(server)
+        .delete(`/api/cards/${body.card.id}`)
+        .set('Authorization', authToken);
+
+      expect(res.status).toBe(204);
+
+      const cardsRes = await request(server)
+        .get(`/api/cards/users/${user.id}`)
+        .set('Authorization', authToken);
+
+      expect(cardsRes.body.cards).toHaveLength(2);
+      done();
+    });
   });
 });
