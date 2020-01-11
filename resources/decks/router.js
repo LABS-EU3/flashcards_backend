4b3564b29e0289a8ff975b48af702d8958df08a8
Noble Obioma <nobioma1@gmail.com>
2020-01-11 20:46:05 +0100

    Remove authorized in deck router

--- resources/decks/router.js
+++ resources/decks/router.js
@@ -10,19 +10,12 @@ const {
 const validate = require('../../utils/validate');
 const { deckSchema } = require('./schema');
 const { deckExists } = require('./middlewares');
-const { authorized, checkId } = require('../global/middlewares');
+const { checkId } = require('../global/middlewares');
 
-router.post('/', authorized, validate(deckSchema), addDeck);
-router.get('/', authorized, getAllDecks);
-router.get('/:id', authorized, checkId, deckExists, getDeck);
-router.put(
-  '/:id',
-  authorized,
-  validate(deckSchema),
-  checkId,
-  deckExists,
-  updateDeck
-);
-router.delete('/:id', authorized, checkId, deckExists, deleteDeck);
+router.post('/', validate(deckSchema), addDeck);
+router.get('/', getAllDecks);
+router.get('/:id', checkId, deckExists, getDeck);
+router.put('/:id', validate(deckSchema), checkId, deckExists, updateDeck);
+router.delete('/:id', checkId, deckExists, deleteDeck);
 
 module.exports = router;
