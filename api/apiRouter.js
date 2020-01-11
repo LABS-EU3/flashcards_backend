b664377a3db51dca9a6fb4bada8cf6d5a3f99e02
Noble Obioma <nobioma1@gmail.com>
2020-01-11 20:43:06 +0100

    Add authorized middleware to deck and card routes

--- api/apiRouter.js
+++ api/apiRouter.js
@@ -3,9 +3,10 @@ const appRouter = require('express').Router();
 const authRouter = require('../resources/auth/router');
 const flashcardsRouter = require('../resources/flashcards/router');
 const deckRouter = require('../resources/decks/router');
+const { authorized } = require('../resources/global/middlewares');
 
 appRouter.use('/auth', authRouter);
-appRouter.use('/cards', flashcardsRouter);
-appRouter.use('/decks', deckRouter);
+appRouter.use('/decks', authorized, deckRouter);
+appRouter.use('/cards', authorized, flashcardsRouter);
 
 module.exports = appRouter;
