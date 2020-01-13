e2718740e7d69fd0b869f46023c18c093b762375
Noble Obioma <nobioma1@gmail.com>
2020-01-13 15:06:17 +0100

    Remove ids from deck seeds

--- data/seeds/01-deck-data.js
+++ data/seeds/01-deck-data.js
@@ -3,13 +3,13 @@ exports.seed = function(knex) {
     .del()
     .then(function() {
       return knex('decks').insert([
-        { id: 1, name: 'Statistical Learning', user_id: 1, public: true },
-        { id: 2, name: 'General Science', user_id: 2, public: false },
-        { id: 3, name: 'Technology ', user_id: 3, public: true },
-        { id: 4, name: 'Biology ', user_id: 4, public: true },
-        { id: 5, name: 'Basic General Knowledge', user_id: 5, public: true },
-        { id: 6, name: 'Famous Personalities', user_id: 6, public: true },
-        { id: 7, name: 'Network Security', user_id: 7, public: false },
+        { name: 'Statistical Learning', user_id: 1, public: true },
+        { name: 'General Science', user_id: 2, public: false },
+        { name: 'Technology ', user_id: 3, public: true },
+        { name: 'Biology ', user_id: 4, public: true },
+        { name: 'Basic General Knowledge', user_id: 5, public: true },
+        { name: 'Famous Personalities', user_id: 6, public: true },
+        { name: 'Network Security', user_id: 7, public: false },
       ]);
     });
 };
