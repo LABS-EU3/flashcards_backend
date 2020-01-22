3ae59a124a8e20c759a08a5f621f5ffb0a7324a8
Oluwajoba <oluwajobabello@gmail.com>
2020-01-22 02:41:40 +0100

    create sessions table / migrations

---
+++ data/migrations/20200122022928_session_table.js
@@ -0,0 +1,23 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('rating', table => {
+    table.increments();
+    table
+      .integer('user_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('users');
+    table
+      .integer('deck_id')
+      .unsigned()
+      .notNullable()
+      .references('full_name')
+      .inTable('users');
+    table.boolean('isComplete').defaultTo(false);
+    table.integer('stopped_index');
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('rating');
+};
