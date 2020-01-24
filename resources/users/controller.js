5db030db892cd1b5344103ce44f7b143d9420cde
Omulosi <mulongojohnpaul@gmail.com>
2020-01-24 12:24:57 +0300

    cleanup

--- resources/users/controller.js
+++ resources/users/controller.js
@@ -5,17 +5,13 @@ exports.deleteUser = async (req, res) => {
     const { subject } = req.decodedToken;
     const { id } = req.params;
 
-    // const user = await model.findBy({ id: subject });
-    console.log(typeof subject);
-    console.log(typeof id);
-
     if (parseInt(id) !== subject) {
       res.status(403).json({ message: 'Request is forbidden' });
       return;
     }
 
     await Users.remove(id);
-    res.status(200).json({ message: 'User successfully deleted' });
+    res.status(200).json({ message: 'user successfully deleted' });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
