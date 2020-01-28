33963463ef28d72adb6602b195a75f4c3e951245
Maaruf Dauda <mkdauda.work@gmail.com>
2020-01-28 16:57:32 +0100

    :construction: Create controller to fetch user score

--- resources/users/controller.js
+++ resources/users/controller.js
@@ -1,4 +1,4 @@
-const Users = require('./model');
+const { remove, getUserRanking } = require('./model');
 
 exports.deleteUser = async (req, res) => {
   try {
@@ -11,9 +11,27 @@ exports.deleteUser = async (req, res) => {
       return;
     }
 
-    await Users.remove(id);
+    await remove(id);
     res.status(200).json({ message: 'user successfully deleted' });
   } catch (error) {
     res.status(500).json({ message: error.message });
+  }
+};
+
+exports.getUserScore = async (req, res) => {
+  try {
+    const { subject } = req.decodedToken;
+    const [user, score] = await getUserRanking(subject);
+
+    const userWithScore = {
+      ...score,
+      ...user,
+    };
+    res.status(200).json({
+      message: `Successfully fetched User score`,
+      data: userWithScore,
+    });
+  } catch (error) {
+    res.status(500).json({ message: `Failed to fetch score.` });
   }
 };
