220a4f9be1868d039fdff20338202547da1becf4
Anna Winther <anski.anna@gmail.com>
2020-01-29 10:43:16 +0000

    feedback endpoint route added

--- api/apiRouter.js
+++ api/apiRouter.js
@@ -4,11 +4,13 @@ const authRouter = require('../resources/auth/router');
 const flashcardsRouter = require('../resources/flashcards/router');
 const deckRouter = require('../resources/decks/router');
 const userRouter = require('../resources/users/router');
+const feedbackRouter = require('../resources/user_feedback/router');
 const { authorized } = require('../resources/global/middlewares');
 
 appRouter.use('/auth', authRouter);
 appRouter.use('/decks', authorized, deckRouter);
 appRouter.use('/cards', authorized, flashcardsRouter);
 appRouter.use('/users', authorized, userRouter);
+appRouter.use('/feedback', authorized, feedbackRouter);
 
 module.exports = appRouter;
--- resources/user_feedback/router.js
+++ resources/user_feedback/router.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
 const { userFeedback } = require('./controller');
-
+// const { checkEmailExists } = require('./middelware');
 const { authorized } = require('../global/middlewares');
 
 const userRouter = express.Router();
 
-userRouter.post('/feedback', authorized, userFeedback);
+userRouter.post('/', authorized, userFeedback);
 
 module.exports = userRouter;
