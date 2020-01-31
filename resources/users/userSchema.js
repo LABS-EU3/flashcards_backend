4bdf5dc6a87b2bb77b37ac67c9033e258da2cafc
Oluwajoba <oluwajobabello@gmail.com>
2020-01-31 10:54:53 +0100

    update user schema to only check for full name / userSchema.js

--- resources/users/userSchema.js
+++ resources/users/userSchema.js
@@ -2,9 +2,6 @@ const joi = require('@hapi/joi');
 
 const updateUserProfileSchema = joi.object({
   fullName: joi.string().required(),
-  email: joi
-    .string()
-    .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
 });
 
 module.exports = {
